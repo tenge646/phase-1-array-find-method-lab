b37a6dda56f60e3fbcd07c0b1024172d89dfa681
kinsman646 <teesam2004@gmail.com>
2023-09-27 07:00:49 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+function superbowlWin(record) {
+    const winningRecord = record.find(game => game.result === "W");
+  
+    if (winningRecord) {
+      return winningRecord.year;
+    } else {
+      return undefined;
+    }
+  }
+  
